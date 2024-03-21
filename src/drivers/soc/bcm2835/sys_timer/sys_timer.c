5d7f97c351de41029bd083260ea97e757f111723
Preston Dresser <pgdresse@uwaterloo.ca>
2024-03-21 17:36:46 -0400

    fix sys timer to use compare reg

--- src/drivers/soc/bcm2835/sys_timer/sys_timer.c
+++ src/drivers/soc/bcm2835/sys_timer/sys_timer.c
@@ -2,6 +2,8 @@
 #include "bcm2835.h"
 
 void sys_timer_sleep(uint32_t microseconds){
-  volatile uint32_t timestamp = SYS_TIMER->CLO;
-  while((SYS_TIMER->CLO - timestamp) < microseconds && (SYS_TIMER->CLO > timestamp));
+  
+  SYS_TIMER->C0 = SYS_TIMER->CLO + microseconds;
+  while(!(SYS_TIMER->CS & 0x01));
+  SYS_TIMER->CS &= ~(0x01);
 }
