b8debcf5daec87f531dad83119243553f15f7a8c
Preston Dresser <pgdresse@uwaterloo.ca>
2024-03-21 17:21:51 -0400

    add interrupt vector

---
+++ src/kernel/isr.c
@@ -0,0 +1,24 @@
+
+void __attribute__((interrupt("UNDEF"))) undefined_instruction_handler(void){
+    while(1);
+}
+
+void __attribute__((interrupt("SWI"))) software_interrupt_handler(void){
+  while(1);
+}
+
+void __attribute__((interrupt("ABORT"))) prefetch_abort_handler(void){
+  while(1);
+}
+
+void __attribute__((interrupt("ABORT"))) data_abort_handler(void){
+  while(1);
+}
+
+void __attribute__((interrupt("IRQ"))) interrupt_handler(void){
+  while(1);
+}
+
+void __attribute__((interrupt("FIQ"))) fast_interrupt_handler(void){
+  while(1);
+}
