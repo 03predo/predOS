1c911dd09cb94d95b11c020a021d557116297059
Preston Dresser <pgdresse@uwaterloo.ca>
2024-06-02 14:17:18 -0400

    initial mmu work

---
+++ src/kernel/mmu.c
@@ -0,0 +1,81 @@
+#include <stdint.h>
+#include "status.h"
+
+#define PAGE_TABLE_SIZE 4096
+
+typedef enum {
+  FAULT = 0b00,
+  COARSE_PAGE_TABLE = 0b01,
+  SECTION = 0b10
+} mmu_first_level_descriptor_type_t;
+
+typedef union {
+  struct {
+    uint32_t descriptor_type : 2;
+    uint32_t bufferable : 1;
+    uint32_t cacheable : 1;
+    uint32_t execute_never : 1;
+    uint32_t domain : 4;
+    uint32_t reserved0 : 1;
+    uint32_t access_permission : 2;
+    uint32_t type_extension : 3;
+    uint32_t access_permission_extension : 1;
+    uint32_t shareable : 1;
+    uint32_t not_global : 1;
+    uint32_t supersection : 1;
+    uint32_t reserved1 : 1;
+    uint32_t section_base_address : 12;
+  } fields;
+  uint32_t raw;
+} mmu_section_descriptor_t;
+
+static uint32_t page_table[PAGE_TABLE_SIZE];
+
+status_t mmu_init(){
+  for(uint32_t i = 0; i < PAGE_TABLE_SIZE; ++i){
+    page_table[i] = 0;
+  }
+
+  mmu_section_descriptor_t section = {
+    .fields = {
+      .descriptor_type = SECTION,
+      .bufferable = 1,
+      .cacheable = 1,
+      .execute_never = 0,
+      .domain = 0,
+      .access_permission = 0b11,
+      .type_extension = 0,
+      .access_permission_extension = 0,
+      .shareable = 0,
+      .not_global = 0,
+      .supersection = 0,
+      .section_base_address = 0x000,
+    }
+  };
+
+  page_table[0] = section.raw;
+
+  section = (mmu_section_descriptor_t) {
+    .fields = {
+      .descriptor_type = SECTION,
+      .bufferable = 1,
+      .cacheable = 0,
+      .execute_never = 1,
+      .domain = 0,
+      .access_permission = 0b11,
+      .type_extension = 0,
+      .access_permission_extension = 0,
+      .shareable = 0,
+      .not_global = 0,
+      .supersection = 0,
+      .section_base_address = 0x200,
+    }
+  };
+
+  page_table[0x200] = section;
+
+  mmu_enable(page_table);
+  
+  return STATUS_OK;
+}
+
